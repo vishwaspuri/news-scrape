433b67c1de86dee5ccf288723a102dca7340e206
vishwaspuri <vishwaspuri09@gmail.com>
2020-04-06 15:22:27 +0530

    Back to SQLite

--- news_api/news_api/settings.py
+++ news_api/news_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-#from .secret_settings import SECRET_KEY, DATABASE_PASSWORD
+from .secret_settings import SECRET_KEY, DATABASE_PASSWORD
 import django_heroku
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-SECRET_KEY = 'c2lvh5np4nfqfna6jm=@*-7(em6_vs21+9o+=%sn%&txls!rqz'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,6 +77,13 @@ WSGI_APPLICATION = 'news_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 '''
 DATABASES = {
     'default': {
@@ -88,10 +95,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }'''
-DATABASES={
-    'default':dj_database_url.config()
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -136,4 +139,3 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Configure Django App for Heroku.
 
-django_heroku.settings(locals())
